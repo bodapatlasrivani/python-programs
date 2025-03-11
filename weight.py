1204c685aa31926cb0c7ad5af45f71c4a30a2fcf
bodapatlasrivani <bodapatlasrivani6@gmail.com>
2025-03-11 13:34:55 +0530

    Create weight.py

---
+++ weight.py
@@ -0,0 +1,11 @@
+weight=int(input("enter weight"))
+height=float(input("enter height"))
+if weight < 18.5:
+  print ("underweight")
+elif x>=18.5 and x<25:
+    print("normal")
+elif x>=25 and x<30:
+  print('over weight')
+elif x>=30:
+  print('obesity')
+  
